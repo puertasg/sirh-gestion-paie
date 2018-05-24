c1d942604702f0db088ce29f1b986970b6c438b2
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-24 14:59:19 +0200

    Ajout ResultatCalculRemuneration

---
+++ src/main/java/dev/paie/entite/ResultatCalculRemuneration.java
@@ -0,0 +1,13 @@
+package dev.paie.entite;
+
+public class ResultatCalculRemuneration {
+
+	private String salaireDeBase;
+	private String salaireBrut;
+	private String totalRetenueSalarial;
+	private String totalCotisationsPatronales;
+	private String netImposable;
+	private String netAPayer;
+
+	// TODO getters & setters à générer
+}
