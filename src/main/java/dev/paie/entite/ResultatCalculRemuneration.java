17f6df32d981e021b032857404d79d0a57cb9a4c
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-24 15:02:02 +0200

    Ajout getters setters

--- src/main/java/dev/paie/entite/ResultatCalculRemuneration.java
+++ src/main/java/dev/paie/entite/ResultatCalculRemuneration.java
@@ -9,5 +9,51 @@ public class ResultatCalculRemuneration {
 	private String netImposable;
 	private String netAPayer;
 
-	// TODO getters & setters à générer
+	public String getSalaireDeBase() {
+		return salaireDeBase;
+	}
+
+	public void setSalaireDeBase(String salaireDeBase) {
+		this.salaireDeBase = salaireDeBase;
+	}
+
+	public String getSalaireBrut() {
+		return salaireBrut;
+	}
+
+	public void setSalaireBrut(String salaireBrut) {
+		this.salaireBrut = salaireBrut;
+	}
+
+	public String getTotalRetenueSalarial() {
+		return totalRetenueSalarial;
+	}
+
+	public void setTotalRetenueSalarial(String totalRetenueSalarial) {
+		this.totalRetenueSalarial = totalRetenueSalarial;
+	}
+
+	public String getTotalCotisationsPatronales() {
+		return totalCotisationsPatronales;
+	}
+
+	public void setTotalCotisationsPatronales(String totalCotisationsPatronales) {
+		this.totalCotisationsPatronales = totalCotisationsPatronales;
+	}
+
+	public String getNetImposable() {
+		return netImposable;
+	}
+
+	public void setNetImposable(String netImposable) {
+		this.netImposable = netImposable;
+	}
+
+	public String getNetAPayer() {
+		return netAPayer;
+	}
+
+	public void setNetAPayer(String netAPayer) {
+		this.netAPayer = netAPayer;
+	}
 }
