11313775a41b80e978620408cf9a03e0badc3a1a
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-25 15:12:08 +0200

    Ajout tests unitaires

--- src/test/java/dev/paie/service/CotisationServiceJpaTest.java
+++ src/test/java/dev/paie/service/CotisationServiceJpaTest.java
@@ -1,5 +1,10 @@
 package dev.paie.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.paie.config.H2Config;
 import dev.paie.config.JpaConfig;
+import dev.paie.entite.Cotisation;
 
 @Configuration
 @ImportResource("jdd-config.xml")
@@ -18,17 +24,26 @@ import dev.paie.config.JpaConfig;
 public class CotisationServiceJpaTest {
 	
 	@Autowired
+	private Cotisation cotisation;
+	
+	@Autowired
 	private CotisationService cotisationService;
 	
 	@Test
 	public void test_sauvegarder_lister_mettre_a_jour()
 	{
 		// TODO sauvegarder une nouvelle cotisation
-		
+		cotisationService.sauvegarder(cotisation);
         // TODO vérifier qu'il est possible de récupérer la nouvelle cotisation via la méthode lister
+		List<Cotisation> listCotisations = cotisationService.lister();
+		assertThat(listCotisations.contains(cotisation));
 
         // TODO modifier une cotisation
+		cotisation.setLibelle("Libelle mis a jour");
+		cotisationService.mettreAJour(cotisation);
 
         // TODO vérifier que les modifications sont bien prises en compte via la méthode lister
+		List<Cotisation> listCotisationUpdate = cotisationService.lister();
+		assertThat(listCotisationUpdate.contains(cotisation));
 	}
 }
