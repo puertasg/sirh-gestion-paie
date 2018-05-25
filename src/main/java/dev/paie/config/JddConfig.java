2f428e853943b57e93187440973b7972844f7f91
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-25 12:55:03 +0200

    Changement config

--- src/main/java/dev/paie/config/JddConfig.java
+++ src/main/java/dev/paie/config/JddConfig.java
@@ -1,12 +1,10 @@
 package dev.paie.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource("classpath:jdd-config.xml")
-@ComponentScan("dev.paie.service")
 public class JddConfig {
 
 }
