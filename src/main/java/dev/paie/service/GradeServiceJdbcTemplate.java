f8278e557bc31911364f0477cff21bfe2bd61e25
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-25 10:18:37 +0200

    Ajout annotation @Autowired

--- src/main/java/dev/paie/service/GradeServiceJdbcTemplate.java
+++ src/main/java/dev/paie/service/GradeServiceJdbcTemplate.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ public class GradeServiceJdbcTemplate implements GradeService{
 	
 	private JdbcTemplate jdbcTemplate;
 	
+	@Autowired
 	public GradeServiceJdbcTemplate(DataSource dataSource) {
 		super();
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
