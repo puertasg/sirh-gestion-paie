9bf148e096a4649b9a2db48586f74cf3bc453dba
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-25 14:54:01 +0200

    Ajout JpaConfig

---
+++ src/main/java/dev/paie/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package dev.paie.config;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class JpaConfig {
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(emf);
+
+		return txManager;
+	}
+}
