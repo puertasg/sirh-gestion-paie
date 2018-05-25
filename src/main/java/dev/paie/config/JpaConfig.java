9673806a962f62a62f27ef9d7c045ee6c6e1d199
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-25 14:58:01 +0200

    Fin ajout JpaConfig

--- src/main/java/dev/paie/config/JpaConfig.java
+++ src/main/java/dev/paie/config/JpaConfig.java
@@ -1,10 +1,13 @@
 package dev.paie.config;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -16,5 +19,22 @@ public class JpaConfig {
 		txManager.setEntityManagerFactory(emf);
 
 		return txManager;
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(true);
+		// activer les logs sql
+		vendorAdapter.setShowSql(true);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		// alternative au persistance.xml
+		factory.setPackagesToScan("dev.paie.entite");
+		factory.setDataSource(dataSource);
+		factory.afterPropertiesSet();
+
+		return factory.getObject();
 	}
 }
