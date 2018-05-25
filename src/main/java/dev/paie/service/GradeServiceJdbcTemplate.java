d55ed028117faeed67ef31040064ca8f88cb2f20
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-25 10:18:20 +0200

    Ajout GradeServiceJdbcTemplate

---
+++ src/main/java/dev/paie/service/GradeServiceJdbcTemplate.java
@@ -0,0 +1,40 @@
+package dev.paie.service;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import dev.paie.entite.Grade;
+
+@Service
+public class GradeServiceJdbcTemplate implements GradeService{
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	public GradeServiceJdbcTemplate(DataSource dataSource) {
+		super();
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@Override
+	public void sauvegarder(Grade nouveauGrade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mettreAJour(Grade grade) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Grade> lister() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
