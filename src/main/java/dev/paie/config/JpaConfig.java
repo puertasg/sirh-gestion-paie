fcdb2d145e951bab61f6d0b23c527668ad6fd9a8
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-28 11:07:10 +0200

    Modification JpaConfig

--- src/main/java/dev/paie/config/JpaConfig.java
+++ src/main/java/dev/paie/config/JpaConfig.java
@@ -1,5 +1,7 @@
 package dev.paie.config;
 
+import java.util.Properties;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -26,7 +28,7 @@ public class JpaConfig {
 	@Bean
 	public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
 		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		vendorAdapter.setGenerateDdl(true);
+		//vendorAdapter.setGenerateDdl(true);
 		// activer les logs sql
 		vendorAdapter.setShowSql(true);
 
@@ -35,6 +37,10 @@ public class JpaConfig {
 		// alternative au persistance.xml
 		factory.setPackagesToScan("dev.paie.entite");
 		factory.setDataSource(dataSource);
+		
+		Properties jpaProperties = new Properties();
+		jpaProperties.setProperty("javax.persistence.schema-generation.database.action", "drop-and-create");
+		factory.setJpaProperties(jpaProperties);
 		factory.afterPropertiesSet();
 
 		return factory.getObject();
