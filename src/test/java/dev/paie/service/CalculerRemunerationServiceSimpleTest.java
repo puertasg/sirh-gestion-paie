235685b6bbd9b95a4c31cb843c249b400e7d025e
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-29 17:44:15 +0200

    Correction erreur Travis

--- src/test/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
+++ src/test/java/dev/paie/service/CalculerRemunerationServiceSimpleTest.java
@@ -10,11 +10,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.paie.config.JddConfig;
+import dev.paie.config.ServicesConfig;
 import dev.paie.entite.BulletinSalaire;
 import dev.paie.entite.ResultatCalculRemuneration;
+import dev.paie.repository.BulletinSalaireRepository;
 
 //Sélection des classes de configuration Spring à utiliser lors du test
-@ContextConfiguration(classes = { CalculerRemunerationServiceSimple.class, JddConfig.class })
+@ContextConfiguration(classes = { ServicesConfig.class, CalculerRemunerationServiceSimple.class, JddConfig.class })
 // Configuration JUnit pour que Spring prenne la main sur le cycle de vie du
 // test
 @RunWith(SpringRunner.class)
@@ -25,6 +27,9 @@ public class CalculerRemunerationServiceSimpleTest {
 	
 	@Autowired
 	private BulletinSalaire bulletin1;
+	
+	@Autowired
+	private BulletinSalaireRepository bulletinRepository;
 
 	@Test
 	public void test_calculer() {
