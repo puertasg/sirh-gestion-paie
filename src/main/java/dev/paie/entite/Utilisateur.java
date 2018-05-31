bfe6c178c14ae0cc0d47195431da8750a940a21f
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-31 11:19:10 +0200

    Ajout entité Utilisateur

---
+++ src/main/java/dev/paie/entite/Utilisateur.java
@@ -0,0 +1,54 @@
+package dev.paie.entite;
+
+public class Utilisateur {
+
+	public enum ROLES {
+		ROLE_ADMINISTRATEUR, ROLE_UTILISATEUR
+	}
+
+	private Integer id;
+	private String nomUtilisateur;
+	private String motDePasse;
+	private Boolean estActif;
+	private ROLES role;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomUtilisateur() {
+		return nomUtilisateur;
+	}
+
+	public void setNomUtilisateur(String nomUtilisateur) {
+		this.nomUtilisateur = nomUtilisateur;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public Boolean getEstActif() {
+		return estActif;
+	}
+
+	public void setEstActif(Boolean estActif) {
+		this.estActif = estActif;
+	}
+
+	public ROLES getRole() {
+		return role;
+	}
+
+	public void setRole(ROLES role) {
+		this.role = role;
+	}
+}
