9ff260a44bae5d3d525dae7e419f4dffcc2418c5
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-31 10:22:41 +0200

    Auto indent

--- src/main/java/dev/paie/web/SecurityWebApplicationInitializer.java
+++ src/main/java/dev/paie/web/SecurityWebApplicationInitializer.java
@@ -1,5 +1,7 @@
 package dev.paie.web;
 
-public class SecurityWebApplicationInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
