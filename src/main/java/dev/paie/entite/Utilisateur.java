c744c4c383853c7e5dccf97631bf2f9328279ef4
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-05-31 11:41:22 +0200

    Ajout 2 users

--- src/main/java/dev/paie/entite/Utilisateur.java
+++ src/main/java/dev/paie/entite/Utilisateur.java
@@ -1,10 +1,12 @@
 package dev.paie.entite;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Utilisateur {
 
 	public enum ROLES {
