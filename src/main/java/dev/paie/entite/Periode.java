ec036a318668943d9fdd71068a899114068c9e42
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-01 11:06:15 +0200

    Correction affichage période

--- src/main/java/dev/paie/entite/Periode.java
+++ src/main/java/dev/paie/entite/Periode.java
@@ -2,6 +2,7 @@ package dev.paie.entite;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.sql.Date;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -13,47 +14,57 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Periode {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id", unique = true, nullable = false)
+	@Column(name = "id", unique = true, nullable = false)
 	private Integer id;
-	
+
 	@Column(name = "DATE_DEBUT", nullable = false)
 	private LocalDate dateDebut;
-	
+
 	@Column(name = "DATE_FIN", nullable = false)
 	private LocalDate dateFin;
-	
-	@OneToMany(mappedBy="periode")
+
+	@OneToMany(mappedBy = "periode")
 	private List<BulletinSalaire> listeBulletins;
-	
+
 	public LocalDate getDateDebut() {
 		return dateDebut;
 	}
+
 	public void setDateDebut(LocalDate dateDebut) {
 		this.dateDebut = dateDebut;
 	}
+
 	public LocalDate getDateFin() {
 		return dateFin;
 	}
+
 	public void setDateFin(LocalDate dateFin) {
 		this.dateFin = dateFin;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
-	public String toString()
-	{
+
+	public Date dateDebutToDate() {
+		return Date.valueOf(this.dateDebut);
+	}
+
+	public Date dateFinToDate() {
+		return Date.valueOf(this.dateFin);
+	}
+
+	public String toString() {
 		String dateDebutFormat = this.dateDebut.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		String dateFinFormat = this.dateFin.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		return dateDebutFormat + " - " + dateFinFormat;
 	}
-	
-	
 
 }
